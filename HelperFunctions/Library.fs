9091f1323659dd7d911a4333da41046f8ec463cf
Joel Castro <joey-92@live.com>
2021-07-08 06:50:55 -0700

    Initial commit

---
+++ HelperFunctions/Library.fs
@@ -0,0 +1,67 @@
+﻿namespace HelperFunctions
+
+open FSharp.Stats
+open FSharp.Stats.Distributions
+open System.Threading.Tasks
+
+module Message =
+    let mutable message = "Hello World"
+
+type Random2DSample (mu: float, std: float, size: int) =
+    // normal distributions with differing standard deviations
+    let normal_0 = Continuous.normal mu std
+    let normal_1 = Continuous.normal mu (std/2.)
+
+    member this.sample0 = Array.init size (fun _ -> normal_0.Sample())
+    member this.sample1 = Array.init size (fun _ -> normal_1.Sample())
+
+type CoordinatePoint (x: float, y:float) =
+    struct
+        member _.X = x
+        member _.Y = y
+    end
+
+module SampleDistribution2D = 
+    let mutable seed = 1
+    Random.SetSampleGenerator(Random.RandThreadSafe(seed))
+
+    let private random2DSample = new Random2DSample(0.0, 1.0, 20000000)
+    let seq0 = Array.map2 (+) random2DSample.sample0 random2DSample.sample1
+    let seq1 = Array.map2 (-) random2DSample.sample0 random2DSample.sample1
+
+    let Step arr size:float = 
+        ((arr |> Array.max) - (arr |> Array.min)) / size
+
+    let LinSpace (arr, size:float) = 
+        let AxisSize = size - 1.0
+        [(arr |> Array.min) .. (Step arr AxisSize) .. (arr |> Array.max)]
+
+    let print arr = 
+        printfn "%A" arr
+
+    let numberOfPoints =
+        seq0 |> Array.length
+
+    let FindGridNodes (axisSeq: List<float>, point:float) =
+        let mutable pivot = axisSeq.Length / 2
+        let mutable upperBound = axisSeq.Length
+        let mutable lowerBound = 0
+
+        while (upperBound - lowerBound > 1) do
+            if point <= axisSeq.[pivot] then
+                upperBound <- pivot
+                pivot <- (pivot - (pivot - lowerBound) /2)
+            elif point >= axisSeq.[pivot] then
+                lowerBound <- pivot
+                pivot <- (pivot + (upperBound - pivot) / 2)
+
+        [|lowerBound; upperBound|]
+
+type Grid (arr0, arr1, size) =
+    let GridSize = size-1.0
+
+    member this.YAxis = (arr1, GridSize) |> SampleDistribution2D.LinSpace
+    member this.XAxis = (arr0, GridSize) |> SampleDistribution2D.LinSpace
+
+    member this.Grid2D = 
+        Array2D.init (this.XAxis.Length + 1) (this.YAxis.Length + 1) (fun x y -> 0.0)
---
+++ KernelDensityEstimation/KDE.cs
@@ -0,0 +1,67 @@
+﻿using HelperFunctions;
+using Microsoft.FSharp.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace KernelDensityEstimation
+{
+    class KDE
+    {
+        static void Main(string[] args)
+        {
+            KDE kde = new();
+
+            kde.CreatePointsSetFromSequences();
+            kde.GenerateGridFromSampleData();
+            kde.LinearBinning();
+        }
+
+        public void GenerateGridFromSampleData()
+        {
+            Grid _ = new(SampleDistribution2D.seq0, SampleDistribution2D.seq1, 100);
+            Grid2D = _.Grid2D;
+            XAxisSeq = _.XAxis;
+            YAxisSeq = _.YAxis;
+        }
+
+        public void LinearBinning()
+        {
+            var watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
+
+            Parallel.ForEach(Points, point =>
+            {
+                int[] xNodes = SampleDistribution2D.FindGridNodes(XAxisSeq, point.X);
+                int XLowerBoundGridIndex = xNodes[0];
+                int XUpperBoundGridIndex = xNodes[1];
+
+                int[] yNodes = SampleDistribution2D.FindGridNodes(YAxisSeq, point.Y);
+                int YLowerBoundGridIndex = yNodes[0];
+                int YUpperBoundGridIndex = yNodes[1];
+
+                lock (Grid2D)
+                {
+                    Grid2D[XLowerBoundGridIndex, YUpperBoundGridIndex] += 1;
+                    Grid2D[XLowerBoundGridIndex, YLowerBoundGridIndex] += 1;
+                    Grid2D[XUpperBoundGridIndex, YLowerBoundGridIndex] += 1;
+                    Grid2D[XUpperBoundGridIndex, YUpperBoundGridIndex] += 1;
+                }
+            });
+            watch.Stop();
+            System.Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
+        }
+
+        public void CreatePointsSetFromSequences()
+        {
+            Points = new List<CoordinatePoint>();
+
+            for (int i = 0; i < SampleDistribution2D.seq0.Length; i++)
+                Points.Add( new CoordinatePoint(SampleDistribution2D.seq0[i], SampleDistribution2D.seq1[i]));
+        }
+
+        private List<CoordinatePoint> Points { get; set; }
+        public double[,] Grid2D { get; set; }
+        private FSharpList<double> XAxisSeq { get; set; }
+        private FSharpList<double> YAxisSeq { get; set; }
+    }
+}
